bf8f3512e0ac76b795f0138b7fc4f9380ff3a1e8
MinecraftPublisher <44898501+MinecraftPublisher@users.noreply.github.com>
2020-07-31 22:34:02 +0430

    Update shortgram.php

--- shortgram.php
+++ shortgram.php
@@ -1,6 +1,6 @@
 <?php
 $new = fopen("chat.txt","w");
-fwrite($new,"💬Rules💬<br>No spam<br>No +18 contents<br>No ADS<br>For ads msg me._._.🔲🔳🔲._._.🔳🔲🔳._._.ℹ️Noticeℹ️<br>Update to latest version if you can't send a message.");
+fwrite($new,"💬Rules💬<br>No spam<br>No +18 contents<br>No ADS<br>For ads msg me._._.🔲🔳🔲<br>Hmmm... I didn't think that anyone would see this...<br>Write 🍆Dead in the chat.._._.🔳🔲🔳._._.ℹ️Noticeℹ️<br>Update to latest version if you can't send a message.<br>Chat is resetted every 2 hours.<br>The timestamp shows the server time.");
 fclose($new);
 echo "Success!";
 
